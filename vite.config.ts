626b2c13d0803ffaa89dffa77e6ea22c6e6a9874
Rick Booth <admin@rickbooth.me>
2021-07-23 12:02:42 +0100

    feat: add library vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+	plugins: [reactRefresh()],
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'lib/index.ts'),
+			name: 'react-component-library',
+		},
+		rollupOptions: {
+			external: ['react'],
+			output: {
+				globals: {
+					react: 'React',
+				},
+				assetFileNames: 'react-component-library.[ext]',
+			},
+		},
+	},
 })

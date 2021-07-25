992654219c10bf3a1fadd516e9f5cf56fd0ebd69
Rick Booth <admin@rickbooth.me>
2021-07-25 19:54:53 +0100

    refactor: create example directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [reactRefresh()],
+	root: 'example',
 	build: {
 		lib: {
 			entry: path.resolve(__dirname, 'lib/index.ts'),
